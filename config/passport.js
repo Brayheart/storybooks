15ec37b3b0a5714a1c26b5c326ddcf28f4159d2d
Tyler Bray <broncobray77@gmail.com>
2018-05-07 13:15:21 -0700

    handled auth

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+const GoogleStratey = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+module.exports = function(passport) {
+    passport.use(
+        new GoogleStratey({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+            proxy: true
+        }, (accessToken, refreshToken, profile, done) => {
+            console.log(accessToken)
+            console.log(profile)
+        })
+    )
+}
