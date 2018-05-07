cbfaae6fbca96b47988b48daddcd62976cf0fa71
Tyler Bray <broncobray77@gmail.com>
2018-05-07 15:35:30 -0700

    adding sessions and authentication

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 //Load User Model
 require('./models/users');
@@ -25,6 +27,24 @@ mongoose.connect(keys.mongoURI)
 
 app.get('/', (req, res) => {
     res.send('It Works!')
+})
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set global vars
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
 })
 
 //Use Routes
