13c24ec9a5d48fddf7ae0567fed599b16911baa5
Tyler Bray <broncobray77@gmail.com>
2018-05-07 13:38:30 -0700

    heroku commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://Tyler:tyler@ds221609.mlab.com:21609/storybooks-dev',
+    googleClientID: '137016868938-nmugu8p43dqep5qmnk4utmubfnro102v.apps.googleusercontent.com',
+    googleClientSecret: 'JQOa10UnnwHEpSU6biBAo-Mr'
+}
