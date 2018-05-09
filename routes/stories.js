1626ff7a29fb12e41d454f2591606dffbeb990ba
Tyler Bray <broncobray77@gmail.com>
2018-05-08 22:42:56 -0700

    show template and format dates w/ moment

--- routes/stories.js
+++ routes/stories.js
@@ -19,6 +19,19 @@ router.get('/', (req, res) => {
         })
 });
 
+//Show Single Story
+router.get('/show:id', (req, res) => {
+    Story.findOne({
+        _id: req.params.id
+    })
+    .populate('user')
+    .then(story => {
+        res.render('stories/show', {
+            story: story
+        });
+    })
+});
+
 //Add Stories Form
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('stories/add');
