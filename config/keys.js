24920a3d6baea52ca5c667ca58a66058e9f251c5
Tyler Bray <broncobray77@gmail.com>
2018-05-10 14:22:39 -0700

    key files

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-
-module.exports = {
-    mongoURI: 'mongodb://Tyler:tyler@ds221609.mlab.com:21609/storybooks-dev',
-    googleClientID: '137016868938-nmugu8p43dqep5qmnk4utmubfnro102v.apps.googleusercontent.com',
-    googleClientSecret: 'JQOa10UnnwHEpSU6biBAo-Mr'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev')
 }
